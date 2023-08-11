d5d2a69450bd3308784b8beb34d2e24c84eaff1b
PC <danyel.16@gmail.com>
2023-08-11 00:30:42 -0600

    Agregar archivos de proyecto.

---
+++ UTSASPMVC/Models/horario.cs
@@ -0,0 +1,14 @@
+﻿namespace UTSASPMVC.Models
+{
+    public class horario
+    {
+
+        public int idaula2 { get; set; }
+        public bool apartado { get; set; }
+        public string dia {get; set; }
+        public string mes { get; set; }
+        public string years { get; set;}
+        public string hora { get; set; }
+
+    }
+}
